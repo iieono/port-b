1ea55047337113febb5584e7ee438c88f788b870
Abdusamadbek Akhamdjonov <axmadjonov.ti@gmail.com>
2024-09-01 22:16:07 +0500

    update

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,25 +1,25 @@
 import { MdPlayArrow } from "react-icons/md";
 export default function Home() {
   return (
-    <div className=" h-full w-full flex items-center lg:items-start justify-start gap-5 lg:justify-between  flex-col p-2 py-5 pt-24 lg:p-5 lg:px-20">
-      <div className="leading-none sharpie-text text-[4rem] w-full px-5 md:text-[8rem] lg:text-[10rem] xl:text-[12rem] 2xl:text-[16rem] flex items-start justify-start flex-col ">
+    <div className=" h-full w-full flex items-center lg:items-start justify-start gap-5 lg:justify-between  flex-col p-2 py-5  lg:p-0 pt-24 lg:pt-20 lg:px-20">
+      <div className="leading-none sharpie-text text-[4rem] w-full px-5 lg:px-10 md:text-[8rem] lg:text-[10rem] xl:text-[14rem] 2xl:text-[18rem] flex items-start justify-start lg:justify-center flex-col ">
         <p className="">
           DESIGNER <span className="text-primary">&</span>
         </p>
         <p>DEVELOPER</p>
       </div>
-      <div className="w-full lg:h-full flex flex-col items-start lg:items-end text-xl lg:text-4xl px-5 lg:px-0 text-primary technor-text font-light justify-end">
-        <p>
-          <span>Hi! I am A. Akhmadjonov.</span>
-          <br />
-          <span>I craft immersive digital</span>
-          <br />
+      <div className="w-full lg:h-full flex flex-col items-start lg:items-start tracking-widest  text-lg lg:text-2xl xl:text-3xl px-5 lg:px-10 text-primary technor-text font-light justify-start">
+        <p className="leading-snug">
+          {/* <span>I am A. Akhmadjonov. </span> */}
+          {/* <br /> */}
+          <span>Crafting immersive digital </span>
+          <br className="md:hidden" />
           <span>solutions that inspire.</span>
         </p>
       </div>
 
-      <div className="flex self-end w-full h-full items-end uppercase justify-center">
-        <div className="absolute bg-primary text-4xl lg:text-8xl bottom-0 right-0 px-8 py-4 gap-3 lg:gap-6 flex items-center rounded-tl-[50px]">
+      <div className="flex self-end w-full items-end uppercase justify-center">
+        <div className="absolute bg-primary text-4xl lg:text-8xl bottom-0 right-0 px-8 py-4 gap-3 lg:gap-6 flex items-center rounded-tl-[40px]">
           <div className="w-8 h-1/3 absolute left-0 -translate-x-full bg-primary bottom-0">
             <div className="w-full h-full bg-custom-red rounded-br-[30px]"></div>
           </div>
