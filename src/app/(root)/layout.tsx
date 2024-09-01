e916541b1793a9ed034e5260fdee859ec3e97fe5
Abdusamadbek Akhamdjonov <axmadjonov.ti@gmail.com>
2024-09-01 17:06:26 +0500

    update

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,19 +1,49 @@
 "use client";
 import { CiLocationArrow1 } from "react-icons/ci";
-import { useState } from "react";
+import { useRef, useState } from "react";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 
 export default function RootGroupLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const [navOpen, setNavOpen] = useState(true);
+  useGSAP(() => {
+    if (window.innerWidth > 1024) {
+      gsap.from(hamRef.current, {
+        duration: 1.5,
+        delay: 2,
+        y: 100,
+        ease: "power2.out",
+      });
+    }
+    gsap.from(mainContainerRef.current, {
+      backgroundColor: "#dc2626",
+      opacity: 0,
+      y: 50,
+      duration: 1.2,
+      delay: 2,
+      ease: "power2.out",
+    });
+  });
+
+  const mainContainerRef = useRef(null);
+  const hamRef = useRef(null);
+  const [navOpen, setNavOpen] = useState(false);
   return (
-    <main className="min-h-screen antialiased bg-custom-red overflow-hidden">
-      <div className="h-screen flex flex-col  p-1 bg-white rounded-b-none rounded-none lg:rounded-[100px] ">
+    <main
+      className="min-h-screen antialiased bg-custom-red overflow-hidden"
+      style={{ perspective: "1000px" }}
+    >
+      <div
+        ref={mainContainerRef}
+        className="h-screen flex flex-col  p-1 bg-white rounded-b-none origin-bottom rounded-none lg:rounded-[100px] "
+      >
         <div className="h-full relative rounded-3xl lg:rounded-[100px]  bg-custom-red">
           {children}
           <div
+            ref={hamRef}
             className="absolute flex items-center  justify-center w-20 h-20 lg:bg-custom-gray top-0 lg:bottom-0 lg:top-auto cursor-pointer right-0 lg:left-0 rounded-full lg:border-4 border-white"
             onClick={() => setNavOpen((prev) => !prev)}
           >
@@ -37,7 +67,7 @@ export default function RootGroupLayout({
             navOpen ? "h-full lg:h-28 rounded-t-none" : "h-0 rounded-t-full"
           } overflow-hidden flex justify-between   transition-all bg-white lg:rounded-t-none lg:bg-transparent duration-700 gap-5 left-0 bottom-0 absolute lg:relative `}
         >
-          <div className="h-full w-full flex flex-col lg:flex-row  items-center justify-center gap-10 lg:gap-0 lg:justify-between px-20 text-4xl uppercase raleway font-light">
+          <div className="h-full w-full flex flex-col lg:flex-row  items-center justify-center gap-10 lg:gap-0 lg:justify-between px-20 text-4xl lg:text-5xl uppercase raleway array-text font-light">
             <div>Home</div>
             <div>About</div>
             <div>Gallery</div>
--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,13 +1,15 @@
 export default function Home() {
   return (
-    <div className=" h-full w-full flex items-center justify-evenly flex-col p-10 lg:px-20 text-9xl">
-      <div className="leading-none sharpie-text text-5xl lg:text-[16rem] flex flex-col ">
+    <div className=" h-full w-full flex items-center lg:items-start lg:justify-between justify-evenly flex-col p-2 lg:p-10 lg:px-20 text-9xl">
+      <div className="leading-none sharpie-text text-[4rem] lg:text-[14rem] flex flex-col ">
         <p className="">
           DESIGNER <span className="text-white">&</span>
         </p>
         <p>DEVELOPER</p>
       </div>
-      <div>hello</div>
+      <div className="w-full flex items-end justify-end">
+        <p>hello</p>
+      </div>
     </div>
   );
 }
