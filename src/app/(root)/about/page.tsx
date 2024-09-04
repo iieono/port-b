5ea00a094f71c6bebf6e2da55e0e1a167b40bafb
Abdusamadbek Akhamdjonov <axmadjonov.ti@gmail.com>
2024-09-04 17:10:04 +0500

    update

--- src/app/(root)/about/page.tsx
+++ src/app/(root)/about/page.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div className="h-full w-full flex justify-between flex-col lg:flex-row p-2 py-5  lg:p-0 pt-24 lg:pt-20 lg:px-20">
+      <div>heloo</div>
+      <div>heloo</div>
+    </div>
+  );
 }
 
 export default About;
--- src/app/(root)/gallery/page.tsx
+++ src/app/(root)/gallery/page.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 function Gallery() {
-  return <div>gallery</div>;
+  return (
+    <div className="h-full w-full flex justify-center items-center p-2 py-5  lg:p-0 pt-24 lg:pt-20 lg:px-20">
+      <div>heloo</div>
+    </div>
+  );
 }
 
 export default Gallery;
